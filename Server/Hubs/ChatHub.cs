c3ef3947072c6df3f1a6d348b49102c9c55409ab
Vladimir Kravchenko <56361236+vlad-kravchenko@users.noreply.github.com>
2020-04-05 10:33:45 +0300

    Added webserver

---
+++ Server/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void NewMessage(string name, string message)
+        {
+            Clients.All.Broadcast(name, message);
+        }
+    }
+}
